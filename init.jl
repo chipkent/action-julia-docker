0ff2ee4c7058bc5bd090d2125100767a596998ca
Chip Kent <5250374+chipkent@users.noreply.github.com>
2021-11-13 22:20:45 -0700

    debugging

--- init.jl
+++ init.jl
@@ -2,15 +2,29 @@ using Pkg
 
 repo = ENV["REPO"]
 branch = ENV["BRANCH"]
+package = last(split(replace(repo,".jl" => ""), "/"))
+
 println("REPO: $repo")
 println("BRANCH: $branch")
+println("PACKAGE: $package")
 
-Pkg.develop(Pkg.PackageSpec(url=repo, rev=branch))
+mkpath("/root/.julia/dev/")
+run(`git clone --branch=$branch $repo /root/.julia/dev/$package`)
+
+println("Activating package: $package")
+Pkg.activate("/root/.julia/dev/$package")
+println("Instantiating package: $package")
+Pkg.instantiate()
+println("Using package: $package")
+@eval using $(Symbol(package))
+
+
+# Pkg.develop(Pkg.PackageSpec(url=repo, rev=branch))
 # Pkg.add(Pkg.PackageSpec(url=repo, rev=branch))
 
-for package in readdir("/root/.julia/dev/")
-    println("Processing package: $package")
-    Pkg.activate("/root/.julia/dev/$package")
-    Pkg.instantiate()
-    @eval using $(Symbol(package))
-end
+# for package in readdir("/root/.julia/dev/")
+#     println("Processing package: $package")
+#     Pkg.activate("/root/.julia/dev/$package")
+#     Pkg.instantiate()
+#     @eval using $(Symbol(package))
+# end
